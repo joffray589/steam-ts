56c6d8febd42d3034f93be3b1f3d95b47c334b0b
joffray <joffray.braga@gmail.com>
2017-03-21 16:47:33 +0100

    add gitignore + code format

--- steam-tradeoffer-manager.d.ts
+++ steam-tradeoffer-manager.d.ts
@@ -4,31 +4,31 @@
 
 declare namespace TradeOfferManager {
     class EconItem {
-        id: string;
-        assetid: string;
-        contextid: string;
-        appid: string;
-        classid: string;
-        instanceid: string;
-        amount: number;
-        pos: number;
-        name: string;
-        market_hash_name: string;
-        name_color: string;
-        background_color: string;
-        type: string;
-        tradable: boolean;
-        marketable: boolean;
-        commodity: boolean;
-        market_tradable_restriction: number;
-        descriptions: any;
-        fraudwarnings: string[];
-        tags: any;
-        app_data: any;
+        id                              : string;
+        assetid                         : string;
+        contextid                       : string;
+        appid                           : string;
+        classid                         : string;
+        instanceid                      : string;
+        amount                          : number;
+        pos                             : number;
+        name                            : string;
+        market_hash_name                : string;
+        name_color                      : string;
+        background_color                : string;
+        type                            : string;
+        tradable                        : boolean;
+        marketable                      : boolean;
+        commodity                       : boolean;
+        market_tradable_restriction     : number;
+        descriptions                    : any;
+        fraudwarnings                   : string[];
+        tags                            : any;
+        app_data                        : any;
 
-        getImageURL(): string;
-        getLargeImageURL(): string;
-        getTag(category: string): any;
+        getImageURL()                   : string;
+        getLargeImageURL()              : string;
+        getTag(category: string)        : any;
     }
 
     /**
@@ -61,140 +61,140 @@ declare namespace TradeOfferManager {
      * https://github.com/DoctorMcKay/node-steam-tradeoffer-manager/blob/master/resources/EResult.js
      */
     enum EResult{
-        Invalid= 0,
-        OK= 1,
-        Fail= 2,
-        NoConnection= 3,
-        InvalidPassword= 5,
-        LoggedInElsewhere= 6,
-        InvalidProtocolVer= 7,
-        InvalidParam= 8,
-        FileNotFound= 9,
-        Busy= 10,
-        InvalidState= 11,
-        InvalidName= 12,
-        InvalidEmail= 13,
-        DuplicateName= 14,
-        AccessDenied= 15,
-        Timeout= 16,
-        Banned= 17,
-        AccountNotFound= 18,
-        InvalidSteamID= 19,
-        ServiceUnavailable= 20,
-        NotLoggedOn= 21,
-        Pending= 22,
-        EncryptionFailure= 23,
-        InsufficientPrivilege= 24,
-        LimitExceeded= 25,
-        Revoked= 26,
-        Expired= 27,
-        AlreadyRedeemed= 28,
-        DuplicateRequest= 29,
-        AlreadyOwned= 30,
-        IPNotFound= 31,
-        PersistFailed= 32,
-        LockingFailed= 33,
-        LogonSessionReplaced= 34,
-        ConnectFailed= 35,
-        HandshakeFailed= 36,
-        IOFailure= 37,
-        RemoteDisconnect= 38,
-        ShoppingCartNotFound= 39,
-        Blocked= 40,
-        Ignored= 41,
-        NoMatch= 42,
-        AccountDisabled= 43,
-        ServiceReadOnly= 44,
-        AccountNotFeatured= 45,
-        AdministratorOK= 46,
-        ContentVersion= 47,
-        TryAnotherCM= 48,
-        PasswordRequiredToKickSession= 49,
-        AlreadyLoggedInElsewhere= 50,
-        Suspended= 51,
-        Cancelled= 52,
-        DataCorruption= 53,
-        DiskFull= 54,
-        RemoteCallFailed= 55,
-        PasswordUnset= 56,
-        PasswordNotSet= 56, // obsolete - renamed to PasswordUnset
-        ExternalAccountUnlinked= 57,
-        PSNTicketInvalid= 58,
-        ExternalAccountAlreadyLinked= 59,
-        RemoteFileConflict= 60,
-        IllegalPassword= 61,
-        SameAsPreviousValue= 62,
-        AccountLogonDenied= 63,
-        CannotUseOldPassword= 64,
-        InvalidLoginAuthCode= 65,
-        AccountLogonDeniedNoMail= 66,
-        AccountLogonDeniedNoMailSent= 66, // obsolete - renamed to AccountLogonDeniedNoMail
-        HardwareNotCapableOfIPT= 67,
-        IPTInitError= 68,
-        ParentalControlRestricted= 69,
-        FacebookQueryError= 70,
-        ExpiredLoginAuthCode= 71,
-        IPLoginRestrictionFailed= 72,
-        AccountLockedDown= 73,
-        AccountLocked= 73, // obsolete - renamed to AccountLockedDown
-        AccountLogonDeniedVerifiedEmailRequired= 74,
-        NoMatchingURL= 75,
-        BadResponse= 76,
-        RequirePasswordReEntry= 77,
-        ValueOutOfRange= 78,
-        UnexpectedError= 79,
-        Disabled= 80,
-        InvalidCEGSubmission= 81,
-        RestrictedDevice= 82,
-        RegionLocked= 83,
-        RateLimitExceeded= 84,
-        AccountLoginDeniedNeedTwoFactor= 85,
-        AccountLogonDeniedNeedTwoFactorCode= 85, // obsolete - renamed to AccountLoginDeniedNeedTwoFactor
-        ItemDeleted= 86,
-        ItemOrEntryHasBeenDeleted= 86, // obsolete - renamed to ItemDeleted
-        AccountLoginDeniedThrottle= 87,
-        TwoFactorCodeMismatch= 88,
-        TwoFactorActivationCodeMismatch= 89,
-        AccountAssociatedToMultiplePartners= 90,
-        AccountAssociatedToMultiplePlayers= 90, // obsolete - renamed to AccountAssociatedToMultiplePartners
-        NotModified= 91,
-        NoMobileDeviceAvailable= 92,
-        TimeIsOutOfSync= 93,
-        SMSCodeFailed= 94,
-        AccountLimitExceeded= 95,
-        TooManyAccountsAccessThisResource= 95, // obsolete - renamed to AccountLimitExceeded
-        AccountActivityLimitExceeded= 96,
-        PhoneActivityLimitExceeded= 97,
-        RefundToWallet= 98,
-        EmailSendFailure= 99,
-        NotSettled= 100,
-        NeedCaptcha= 101,
-        GSLTDenied= 102,
-        GSOwnerDenied= 103,
-        InvalidItemType= 104,
-        IPBanned= 105
+        Invalid                                 = 0,
+        OK                                      = 1,
+        Fail                                    = 2,
+        NoConnection                            = 3,
+        InvalidPassword                         = 5,
+        LoggedInElsewhere                       = 6,
+        InvalidProtocolVer                      = 7,
+        InvalidParam                            = 8,
+        FileNotFound                            = 9,
+        Busy                                    = 10,
+        InvalidState                            = 11,
+        InvalidName                             = 12,
+        InvalidEmail                            = 13,
+        DuplicateName                           = 14,
+        AccessDenied                            = 15,
+        Timeout                                 = 16,
+        Banned                                  = 17,
+        AccountNotFound                         = 18,
+        InvalidSteamID                          = 19,
+        ServiceUnavailable                      = 20,
+        NotLoggedOn                             = 21,
+        Pending                                 = 22,
+        EncryptionFailure                       = 23,
+        InsufficientPrivilege                   = 24,
+        LimitExceeded                           = 25,
+        Revoked                                 = 26,
+        Expired                                 = 27,
+        AlreadyRedeemed                         = 28,
+        DuplicateRequest                        = 29,
+        AlreadyOwned                            = 30,
+        IPNotFound                              = 31,
+        PersistFailed                           = 32,
+        LockingFailed                           = 33,
+        LogonSessionReplaced                    = 34,
+        ConnectFailed                           = 35,
+        HandshakeFailed                         = 36,
+        IOFailure                               = 37,
+        RemoteDisconnect                        = 38,
+        ShoppingCartNotFound                    = 39,
+        Blocked                                 = 40,
+        Ignored                                 = 41,
+        NoMatch                                 = 42,
+        AccountDisabled                         = 43,
+        ServiceReadOnly                         = 44,
+        AccountNotFeatured                      = 45,
+        AdministratorOK                         = 46,
+        ContentVersion                          = 47,
+        TryAnotherCM                            = 48,
+        PasswordRequiredToKickSession           = 49,
+        AlreadyLoggedInElsewhere                = 50,
+        Suspended                               = 51,
+        Cancelled                               = 52,
+        DataCorruption                          = 53,
+        DiskFull                                = 54,
+        RemoteCallFailed                        = 55,
+        PasswordUnset                           = 56,
+        PasswordNotSet                          = 56, // obsolete - renamed to PasswordUnset
+        ExternalAccountUnlinked                 = 57,
+        PSNTicketInvalid                        = 58,
+        ExternalAccountAlreadyLinked            = 59,
+        RemoteFileConflict                      = 60,
+        IllegalPassword                         = 61,
+        SameAsPreviousValue                     = 62,
+        AccountLogonDenied                      = 63,
+        CannotUseOldPassword                    = 64,
+        InvalidLoginAuthCode                    = 65,
+        AccountLogonDeniedNoMail                = 66,
+        AccountLogonDeniedNoMailSent            = 66, // obsolete - renamed to AccountLogonDeniedNoMail
+        HardwareNotCapableOfIPT                 = 67,
+        IPTInitError                            = 68,
+        ParentalControlRestricted               = 69,
+        FacebookQueryError                      = 70,
+        ExpiredLoginAuthCode                    = 71,
+        IPLoginRestrictionFailed                = 72,
+        AccountLockedDown                       = 73,
+        AccountLocked                           = 73, // obsolete - renamed to AccountLockedDown
+        AccountLogonDeniedVerifiedEmailRequired = 74,
+        NoMatchingURL                           = 75,
+        BadResponse                             = 76,
+        RequirePasswordReEntry                  = 77,
+        ValueOutOfRange                         = 78,
+        UnexpectedError                         = 79,
+        Disabled                                = 80,
+        InvalidCEGSubmission                    = 81,
+        RestrictedDevice                        = 82,
+        RegionLocked                            = 83,
+        RateLimitExceeded                       = 84,
+        AccountLoginDeniedNeedTwoFactor         = 85,
+        AccountLogonDeniedNeedTwoFactorCode     = 85, // obsolete - renamed to AccountLoginDeniedNeedTwoFactor
+        ItemDeleted                             = 86,
+        ItemOrEntryHasBeenDeleted               = 86, // obsolete - renamed to ItemDeleted
+        AccountLoginDeniedThrottle              = 87,
+        TwoFactorCodeMismatch                   = 88,
+        TwoFactorActivationCodeMismatch         = 89,
+        AccountAssociatedToMultiplePartners     = 90,
+        AccountAssociatedToMultiplePlayers      = 90, // obsolete - renamed to AccountAssociatedToMultiplePartners
+        NotModified                             = 91,
+        NoMobileDeviceAvailable                 = 92,
+        TimeIsOutOfSync                         = 93,
+        SMSCodeFailed                           = 94,
+        AccountLimitExceeded                    = 95,
+        TooManyAccountsAccessThisResource       = 95, // obsolete - renamed to AccountLimitExceeded
+        AccountActivityLimitExceeded            = 96,
+        PhoneActivityLimitExceeded              = 97,
+        RefundToWallet                          = 98,
+        EmailSendFailure                        = 99,
+        NotSettled                              = 100,
+        NeedCaptcha                             = 101,
+        GSLTDenied                              = 102,
+        GSOwnerDenied                           = 103,
+        InvalidItemType                         = 104,
+        IPBanned                                = 105
     }
 
     /**
      * https://github.com/DoctorMcKay/node-steam-tradeoffer-manager/wiki/TradeOffer
      */
     class TradeOffer{
-        manager:                TradeOfferManager;
-        id:                     string;
-        partner:                SteamID;
-        message:                string;
-        state:                  ETradeOfferState;
-        itemsToGive:            EconItem[];
-        itemsToReceive:         EconItem[];
-        isOurOffer:             boolean;
-        created:                Date;
-        updated:                Date;
-        expires:                Date;
-        tradeID:                string;
-        fromRealTimeTrade:      boolean;
-        confirmationMethod:     EConfirmationMethod;
-        escrowEnds:             Date;
-        rawJson:                any;
+        manager                 : TradeOfferManager;
+        id                      : string;
+        partner                 : SteamID;
+        message                 : string;
+        state                   : ETradeOfferState;
+        itemsToGive             : EconItem[];
+        itemsToReceive          : EconItem[];
+        isOurOffer              : boolean;
+        created                 : Date;
+        updated                 : Date;
+        expires                 : Date;
+        tradeID                 : string;
+        fromRealTimeTrade       : boolean;
+        confirmationMethod      : EConfirmationMethod;
+        escrowEnds              : Date;
+        rawJson                 : any;
 
         isGlitched(): boolean;
 
@@ -251,23 +251,23 @@ declare namespace TradeOfferManager {
      * https://github.com/DoctorMcKay/node-steam-tradeoffer-manager/blob/master/resources/EOfferFilter.js
      */
     enum EOfferFilter{
-        ActiveOnly = 1,
-        HistoricalOnly = 2,
-        All = 3
+        ActiveOnly      = 1,
+        HistoricalOnly  = 2,
+        All             = 3
     }
 
     interface Options{
-        steam?:                     any;
-        community?:                 SteamCommunity;
-        domain?:                    string;
-        language?:                  string;
-        pollInterval?:              number;
-        cancelTime?:                number;
-        pendingCancelTime?:         number;
-        cancelOfferCount?:          number;
-        cancelOfferCountMinAge?:    number;
-        globalAssetCache?:          boolean;
-        pollData?:                  string;
+        steam                       ?: any;
+        community                   ?: SteamCommunity;
+        domain                      ?: string;
+        language                    ?: string;
+        pollInterval                ?: number;
+        cancelTime                  ?: number;
+        pendingCancelTime           ?: number;
+        cancelOfferCount            ?: number;
+        cancelOfferCountMinAge      ?: number;
+        globalAssetCache            ?: boolean;
+        pollData                    ?: string;
     }
 
 }
@@ -279,16 +279,28 @@ declare namespace TradeOfferManager {
  */
 declare class TradeOfferManager {
 
-    pollInterval: number;
-    cancelTime: number;
-    pendingCancelTime: number;
-    cancelOfferCount: number;
-    cancelOfferCountMinAge: number;
-    pollData: any;
-    apiKey: string;
-    steamID: SteamID;
+    pollInterval            : number;
+    cancelTime              : number;
+    pendingCancelTime       : number;
+    cancelOfferCount        : number;
+    cancelOfferCountMinAge  : number;
+    pollData                : any;
+    apiKey                  : string;
+    steamID                 : SteamID;
 
     constructor(options: TradeOfferManager.Options);
+
+
+    on(event: 'newOffer',                   callback: (offer: TradeOfferManager.TradeOffer) => any);
+    on(event: 'sentOfferChanged',           callback: (offer: TradeOfferManager.TradeOffer, oldState: TradeOfferManager.ETradeOfferState) => any);
+    on(event: 'sentOfferCanceled',          callback: (offer: TradeOfferManager.TradeOffer, reason: string) => any);
+    on(event: 'sentPendingOfferCanceled',   callback: (offer: TradeOfferManager.TradeOffer) => any);
+    on(event: 'unknownOfferSent',           callback: (offer: TradeOfferManager.TradeOffer) => any);
+    on(event: 'receivedOfferChanged',       callback: (offer: TradeOfferManager.TradeOffer, oldState: TradeOfferManager.ETradeOfferState) => any);
+    on(event: 'pollFailure',                callback: (err: Error) => any);
+    on(event: 'pollSuccess',                callback: () => any);
+    on(event: 'pollData',                   callback: (pollData: any) => any);
+
 
     on(event: string, callback: any);
 
@@ -308,12 +320,12 @@ declare class TradeOfferManager {
         filter: TradeOfferManager.EOfferFilter,
         callback: (err: Error, sent: TradeOfferManager.TradeOffer[], received: TradeOfferManager.TradeOffer[] ) => any
     );
+
     getOffers(
         filter: TradeOfferManager.EOfferFilter,
         historicalCutoff: Date,
         callback: (err: Error, sent: TradeOfferManager.TradeOffer[], received: TradeOfferManager.TradeOffer[] ) => any
     );
-
 
     loadInventory(
         appid: string,
@@ -332,11 +344,11 @@ declare class TradeOfferManager {
 
     getOfferToken(callback: (err: Error, token: string) => any);
 
-
     getEscrowDuration(
         steamID: SteamID,
         callback: (err: Error, daysTheirEscrow: number, daysMyEscrow: number) => any
     );
+
     getEscrowDuration(
         steamID: SteamID,
         token: string,
